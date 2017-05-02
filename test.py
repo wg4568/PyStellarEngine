2cf0e32bac47f6bbd45ef0db9e0b6396b85b45f6
wg4568 <wg4568@gmail.com>
2017-05-02 15:13:42 -0700

    Added noise library

--- test.py
+++ test.py
@@ -1,38 +1,11 @@
-# #for python 2.7.13
+import stellar
 
-from stellar import objects, sprites, rooms, base
-from stellar.keys import *
+game = stellar.base.Base()
+game.title = "Hello, world!"
 
-game = base.Base()
-
-
-class Player(objects.Object):
-	def __init__(self):
-		objects.Object.__init__(self)
-
-		spr = sprites.Box((255, 0, 0), 10, 10)
-		self.add_sprite("box", spr)
-		self.set_sprite("box")
-
-	def logic(self):
-		print self.mouse_over()
-
-	def control(self, buttons, mousepos):
-		if buttons[K_UP]:
-			self.y -= 1
-		if buttons[K_DOWN]:
-			self.y += 1
-		if buttons[K_LEFT]:
-			self.x -= 1
-		if buttons[K_RIGHT]:
-			self.x += 1
-
-obj_player = Player()
-
-rm_main = rooms.Room()
-rm_main.add_object(obj_player)
-
-game.add_room("rm_main", rm_main)
-game.set_room("rm_main")
+room = stellar.rooms.Room()
+room.background = (180, 0, 0)
+game.add_room("main", room)
+game.set_room("main")
 
 game.start()
