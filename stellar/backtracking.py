344ef3d1769f462fc4517ef15b086a9a665cbaba
Ramaraunt <ramaraunt1@gmail.com>
2017-05-02 13:23:08 -0400

    more maze generation stuff

--- stellar/backtracking.py
+++ stellar/backtracking.py
@@ -21,29 +21,47 @@ class Node:
         self.neighbors[direction]=neighbor
 
 class Grid:
-    def __init__(self, width=30, height=30):
+    def __init__(self):
+        self.nodes = None
+        
+    def createCells(self, width=30, height=30):
         self.nodes = [[Node()] * width] * height]
+        self.width = width
+        self.height = height
         for n in range(0,width-1):
             for m in range(0,height-1):
-                self.nodes[n,m].row = n
-                self.nodes[n,m].column = m
+                self.nodes[n][m].row = n
+                self.nodes[n][m].column = m
         for n in range(0,width-1):
             for m in range(0,height-1):
                 if n > 0:
-                    self.nodes[n,m].addNeighbor(1,self.nodes[n-1,m])
+                    self.nodes[n][m].addNeighbor(1,self.nodes[n-1][m])
                 if m > 0:
-                    self.nodes[n,m].addNeighbor(0,self.nodes[n,m-1])
+                    self.nodes[n][m].addNeighbor(0,self.nodes[n][m-1])
                 if n < width - 1:
-                    self.nodes[n,m].addNeighbor(3,self.nodes[n+1,m])
+                    self.nodes[n][m].addNeighbor(3,self.nodes[n+1][m])
                 if m < height - 1:
-                    self.nodes[n,m].addNeighbor(2,self.nodes[n,m+1])
+                    self.nodes[n][m].addNeighbor(2,self.nodes[n][m+1])
+
+      
         
 def buildMaze(grid):
     seed()
+    side = random.range(1)
+    cap = random.range(1)
+    if cap == 1:
+        startM = 0
+    else:
+        startM = grid.height - 1
+    if side == 1:
+        startN = 0
+    else:
+        startN = grid.width - 1
+
     #wip
-    
-    
-        
 
         
+    
+
+
     
