bbfcb3097296f6590a78fb83c6e3c1f71a4e5d42
Ramaraunt <ramaraunt1@gmail.com>
2017-05-03 15:22:32 -0400

    Revert "Added new object system"
    This reverts commit c77f49ca0b3ee000f6c7d6efb2f7f8af4f7a49bb.

--- stellar/objects.py
+++ stellar/objects.py
@@ -1,19 +1,12 @@
 class Object:
-	def __init__(self, x=0, y=0, scale=1.0 persistent=False, runsInBackground=False, priority=0):
+	def __init__(self, x=0, y=0, scale=1.0):
 		self.x = x
 		self.y = y
-		self.start_x = x
-		self.start_y = y
 		self.scale = scale
 		self.room = None
-		self.persistent = False
-		self.runsInBackground = False
-		self.priority=priority
-		self.type = "Object_Base"
 
-		self.nodes = []
-		self.sprites = []
-
+		self.current_sprite = None
+		self.sprites = {}
 
 	def room_link(self, room):
 		self.room = room
@@ -41,219 +34,19 @@ class Object:
 		self.x += x
 		self.y += y
 
-	def _control(self, buttons, mouse):
-                for node in nodes:
-                        node._control(buttons, mouse)
-
-                for sprite in sprites:
-                        sprite._control(buttons, mouse)
-
-		try: self.control(buttons, mouse)
-		except AttributeError: pass
-
-	def _early_control(self, buttons, mouse):
-                for node in nodes:
-                        node._early_control(buttons, mouse)
-
-                for sprite in sprites:
-                        sprite._early_control(buttons, mouse)
-                        
-		try: self.early_control(buttons, mouse)
-		except AttributeError: pass
-
-	def _late_control(self, buttons, mouse):
-                for node in nodes:
-                        node._late_control(buttons, mouse)
-
-                for sprite in sprites:
-                        sprite._late_control(buttons, mouse)
-                        
-		try: self.early_control(buttons, mouse)
-		except AttributeError: pass
-
-	def _gui_control(self, buttons, mouse):
-                for node in nodes:
-                        node._gui_control(buttons, mouse)
-
-                for sprite in sprites:
-                        sprite._gui_control(buttons, mouse)
-                
-                try: self.gui_control(buttons, mouse)
-                except AttributeError: pass
-
-        def _physics_control(self, buttons, mouse):
-                for node in nodes:
-                        node._physics_control(buttons, mouse)
-
-                for sprite in sprites:
-                        sprite._physics_control(buttons, mouse)
-                                
-                try: self.physics_control(buttons, mouse)
-                except AttributeError: pass
-
-class GUI(Object):
-        def __init__(self, x=0, y=0, scale=1.0, persistent=False, runsInBackground=False):
-                Object.__init__(self, x, y, scale, persistent, runsInBackground)
-                self.type = "Object_GUI"
-
-        def _control(self, buttons, mouse):
-                for node in nodes:
-                        node._control(buttons, mouse)
-
-                for sprite in sprites:
-                        sprite._control(buttons, mouse)
-
-
-	def _early_control(self, buttons, mouse):
-                for node in nodes:
-                        node._early_control(buttons, mouse)
-
-                for sprite in sprites:
-                        sprite._early_control(buttons, mouse)
-
-	def _late_control(self, buttons, mouse):
-                for node in nodes:
-                        node._late_control(buttons, mouse)
-
-                for sprite in sprites:
-                        sprite._late_control(buttons, mouse)
-
-	def _gui_control(self, buttons, mouse):
-                for node in nodes:
-                        node._gui_control(buttons, mouse)
-
-                for sprite in sprites:
-                        sprite._gui_control(buttons, mouse)
-                        
-                try: self.physics_control(buttons, mouse)
-                except AttributeError: pass
-
-        def _physics_control(self, buttons, mouse):
-                pass
-
-class Rigidbody(Object):
-        def __init__(self, x=0, y=0, scale=1.0, persistent=False, runsInBackground=False):
-                Object.__init__(self, x, y, scale, persistent, runsInBackground)
-                self.type = "Object_Rigidbody"
-
-        def _control(self, buttons, mouse):
-                for node in nodes:
-                        node._control(buttons, mouse)
-
-                for sprite in sprites:
-                        sprite._control(buttons, mouse)
-
-
-	def _early_control(self, buttons, mouse):
-                for node in nodes:
-                        node._early_control(buttons, mouse)
-
-                for sprite in sprites:
-                        sprite._early_control(buttons, mouse)
-
-	def _late_control(self, buttons, mouse):
-                for node in nodes:
-                        node._late_control(buttons, mouse)
-
-                for sprite in sprites:
-                        sprite._late_control(buttons, mouse)
-
-	def _gui_control(self, buttons, mouse):
-                 for node in nodes:
-                        node._gui_control(buttons, mouse)
-
-                for sprite in sprites:
-                        sprite._gui_control(buttons, mouse)
-        
-        def _physics_control(self, buttons, mouse):
-                for node in nodes:
-                        node._physics_control(buttons, mouse)
-
-                for sprite in sprites:
-                        sprite._physics_control(buttons, mouse)
-                
-                try: self.physics_control(buttons, mouse)
-                except AttributeError: pass
-
-class Static(Object):
-        def __init__(self, x=0, y=0, scale=1.0, persistent=False, runsInBackground=False):
-                Object.__init__(self, x, y, scale, persistent, runsInBackground)
-                self.type = "Object_Rigidbody"
-                
-	def _control(self, buttons, mouse):
-                for node in nodes:
-                        node._control(buttons, mouse)
-
-                for sprite in sprites:
-                        sprite._control(buttons, mouse)
-                        
-		try: self.control(buttons, mouse)
-		except AttributeError: pass
-
-	def _early_control(self, buttons, mouse):
-                for node in nodes:
-                        node._early_control(buttons, mouse)
-
-                for sprite in sprites:
-                        sprite._early_control(buttons, mouse)
-                        
-		try: self.early_control(buttons, mouse)
-		except AttributeError: pass
-
-	def _late_control(self, buttons, mouse):
-                for node in nodes:
-                        node._late_control(buttons, mouse)
-
-                for sprite in sprites:
-                        sprite._late_control(buttons, mouse)
-
-		try: self.early_control(buttons, mouse)
-		except AttributeError: pass
-
-	def _gui_control(self, buttons, mouse):
-                for node in nodes:
-                        node._gui_control(buttons, mouse)
-
-                for sprite in sprites:
-                        sprite._gui_control(buttons, mouse)
-        
-        def _physics_control(self, buttons, mouse):
-                pass
-
-class Empty(Object):
-        def __init__(self, x=0, y=0, scale=1.0):
-                Object.__init__(self, x, y, scale, False, False)
-                self.type = "Object_Empty"
+	def _logic(self):
+		func = getattr(self, "logic", None)
+		if callable(func):
+			self.logic()
 
 	def _control(self, buttons, mouse):
-                for node in nodes:
-                        node._control(buttons, mouse)
+		func = getattr(self, "control", None)
+		if callable(func):
+			self.control(buttons, mouse)
 
-                for sprite in sprites:
-                        sprite._control(buttons, mouse)
+	def _draw(self):
+		self.get_current_sprite().draw(self.room, self.get_position(), self.scale)
 
-
-	def _early_control(self, buttons, mouse):
-                for node in nodes:
-                        node._early_control(buttons, mouse)
-
-                for sprite in sprites:
-                        sprite._early_control(buttons, mouse)
-
-	def _late_control(self, buttons, mouse):
-                for node in nodes:
-                        node._late_control(buttons, mouse)
-
-                for sprite in sprites:
-                        sprite._late_control(buttons, mouse)
-
-	def _gui_control(self, buttons, mouse):
-                for node in nodes:
-                        node._gui_control(buttons, mouse)
-
-                for sprite in sprites:
-                        sprite._gui_control(buttons, mouse)
-        
-        def _physics_control(self, buttons, mouse):
-                pass
-
+		func = getattr(self, "draw", None)
+		if callable(func):
+			self.draw()
